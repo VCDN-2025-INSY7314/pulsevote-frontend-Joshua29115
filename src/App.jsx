fca8f8117f3aca81bd4caf6e3fb5cc914bbdc8a3
Joshua Pillay <jrpillay7@gmail.com>
2025-07-24 14:03:29 +0200

    Added SSL setup for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/test')
+      .then(response => setData(response.data.message))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <>
       <h2>Welcome to PulseVote</h2>
+      <p>{data}</p>
     </>
   )
 }
